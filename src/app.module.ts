658dbee6f8ccc7c4812a36b32ec95595a42ae01d
duvanflor <duvan.flor@netto.com.co>
2024-12-19 13:19:38 -0500

    Fix: Setup database in local

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { envs } from '@/common';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: envs.postGresHost,
+      port: envs.postGresPort,
+      username: envs.postGresUser,
+      password: envs.postGresPassword,
+      database: envs.postGresDb,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/common/config/envs.ts
+++ src/common/config/envs.ts
@@ -8,6 +8,11 @@ interface EnvVars {
   API_KEY_PRV_WOMPO: string;
   API_KEY_EVENTS_WOMPO: string;
   API_KEY_INTEGRITY_WOMPO: string;
+  POSTGRES_HOST: string;
+  POSTGRES_PORT: number;
+  POSTGRES_DB: string;
+  POSTGRES_USER: string;
+  POSTGRES_PASSWORD: string;
 }
 
 const envsSchema = joi
@@ -18,6 +23,11 @@ const envsSchema = joi
     API_KEY_PRV_WOMPO: joi.string().required(),
     API_KEY_EVENTS_WOMPO: joi.string().required(),
     API_KEY_INTEGRITY_WOMPO: joi.string().required(),
+    POSTGRES_HOST: joi.string().required(),
+    POSTGRES_PORT: joi.number().required(),
+    POSTGRES_DB: joi.string().required(),
+    POSTGRES_USER: joi.string().required(),
+    POSTGRES_PASSWORD: joi.string().required(),
   })
   .unknown(true);
 
@@ -36,4 +46,9 @@ export const envs = {
   apiKeyPrvWompo: envVars.API_KEY_PRV_WOMPO,
   apiKeyEventsWompo: envVars.API_KEY_EVENTS_WOMPO,
   apiKeyIntegrityWompo: envVars.API_KEY_INTEGRITY_WOMPO,
+  postGresHost: envVars.POSTGRES_HOST,
+  postGresPort: envVars.POSTGRES_PORT,
+  postGresDb: envVars.POSTGRES_DB,
+  postGresUser: envVars.POSTGRES_USER,
+  postGresPassword: envVars.POSTGRES_PASSWORD,
 };
